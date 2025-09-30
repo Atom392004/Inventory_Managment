afaa4730de15a6887c5cd0d7d948a81cbf3b88af
Abhayhindocha <121329454+Abhayhindocha@users.noreply.github.com>
2025-09-30 17:01:25 +0530

    final phase left

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:QC%402025@localhost:5432/Inventory_mang")
     API_SECRET_KEY: str = os.getenv("API_SECRET_KEY")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1440"))  # 24 hours
     
